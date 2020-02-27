bf2b9542390c4f13c05ad8c122e5e11fa2fb2a5d
Drew Daddio <DrewDaddio@users.noreply.github.com>
2020-02-26 19:14:20 -0800

    Add files via upload

---
+++ Pointers/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "Pointers are the types of objects with which we indirectly access other objects" << endl;
+
+    int *p; //This is type pointer to an integer
+
+    cout << "assigning a value directly to a pointer is not allowed" << endl;
+
+    int x;
+
+    p = &x; // This is interpreted as an address of x.
+
+
+
+
+}
